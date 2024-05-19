34928812b781c017628437ad2cdbdaf6c44a13f2
mthomas <mthomas@conceptial.tech>
2024-05-19 16:12:01 -0700

    Added KeyCloak Authentication, and test working okay.

--- src/ThirdPartyFreight.Web/Program.cs
+++ src/ThirdPartyFreight.Web/Program.cs
@@ -8,6 +8,19 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:28081") });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.MetadataUrl = "http://localhost:18080/realms/csd-tpf/.well-known/openid-configuration";
+    options.ProviderOptions.Authority = "http://localhost:18080/realms/csd-tpf";
+    options.ProviderOptions.ClientId = "csd-tpf-web";
+    options.ProviderOptions.ResponseType = "id_token token";
+    
+    options.UserOptions.NameClaim = "preferred_username";
+    options.UserOptions.RoleClaim = "client roles"; 
+    options.UserOptions.ScopeClaim = "scope";
+});
+
 builder.Services.AddTelerikBlazor();
 builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
