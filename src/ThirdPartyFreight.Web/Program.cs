3b2e9306bb3edfc7f1419ddb0437779f4320e113
Michael Thomas <mthomas@beckman.com>
2024-05-21 10:46:49 -0700

    The most significant changes include the update to the `docker-compose.override.yml` file to expose specific ports for the service, the modification of the `docker-compose.yml` file to enable metrics for the Keycloak service, and the alteration of the `launchSettings.json` file to change the application URLs for both HTTP and HTTPS protocols.
    1. The `docker-compose.override.yml` file was updated to expose ports 38080 and 39443 for the service. This change allows these ports to be accessible for the service, which can be crucial for its operation and communication with other services or applications.
    
    2. The `docker-compose.yml` file was updated to enable metrics for the Keycloak service. This change allows the monitoring and collection of metrics from the Keycloak service, which can be useful for performance analysis and troubleshooting.
    
    3. The `launchSettings.json` file was updated to change the application URLs for both HTTP and HTTPS protocols. This change alters the URLs that the application uses for communication, which can affect how the application is accessed and how it interacts with other services or applications.
    
    4. The `Dockerfile` was updated to change the password for the Telerik NuGet feed and to remove the command to start Nginx in daemon mode. This change affects the security of the Telerik NuGet feed and the operation of Nginx within the Docker container.
    
    5. The `Program.cs` file was updated to remove the claim for client roles in the OIDC authentication setup. This change affects the authentication process for the application, potentially altering its security and access control.
    
    6. The `ThirdPartyFreight.Web.csproj` file was updated to remove all references to the `Assets` directory and to remove the inclusion of a `.DS_Store` file in the `Assets\Telerik5.1.1` directory. This change affects the project's structure and the inclusion of certain files in the build.

--- src/ThirdPartyFreight.Web/Program.cs
+++ src/ThirdPartyFreight.Web/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddOidcAuthentication(options =>
     options.ProviderOptions.ResponseType = "id_token token";
     
     options.UserOptions.NameClaim = "preferred_username";
-    options.UserOptions.RoleClaim = "client roles"; 
     options.UserOptions.ScopeClaim = "scope";
 });
 builder.Services.AddSingleton<Home.IssuesGenerator>();
