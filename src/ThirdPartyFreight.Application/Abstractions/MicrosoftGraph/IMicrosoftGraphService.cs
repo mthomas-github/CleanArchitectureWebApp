dfc04c57cde485664c5210a52c931ad2bd2d003e
mthomas <mthomas@beckman.com>
2024-06-12 12:58:27 -0700

    added logic to call Microsoft Graph QL

---
+++ src/ThirdPartyFreight.Application/Abstractions/MicrosoftGraph/IMicrosoftGraphService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Graph.Models;
+
+namespace ThirdPartyFreight.Application.Abstractions.MicrosoftGraph;
+public interface IMicrosoftGraphService
+{
+    Task<User?> GetUser(string userPrincipalName);
+
+    Task<(IEnumerable<Site>?, IEnumerable<Site>?)> GetSharepointSites();
+}
---
+++ src/ThirdPartyFreight.Infrastructure/MicrosoftGraph/MicrosoftGraphService.cs
@@ -0,0 +1,67 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using ThirdPartyFreight.Application.Abstractions.MicrosoftGraph;
+
+namespace ThirdPartyFreight.Infrastructure.MicrosoftGraph;
+
+public class MicrosoftGraphService(string tenantId, string clientId, string clientSecret) : IMicrosoftGraphService
+{
+    private GraphServiceClient GraphClient { get; set; } = CreateGraphClient(tenantId, clientId, clientSecret);
+    private static GraphServiceClient CreateGraphClient(string tenantId, string clientId, string clientSecret)
+    {
+        var options = new TokenCredentialOptions
+        {
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+        };
+        var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, options);
+
+        string[] scopes = ["https://graph.microsoft.com/.default"];
+
+        return new GraphServiceClient(clientSecretCredential, scopes);
+    }
+
+    public async Task<User?> GetUser(string userPrincipalName)
+    {
+        return await GraphClient.Users[userPrincipalName].GetAsync();
+    }
+
+    public async Task<(IEnumerable<Site>?, IEnumerable<Site>?)> GetSharepointSites()
+    {
+        List<Site>? sites = (await GraphClient.Sites.GetAllSites.GetAsGetAllSitesGetResponseAsync())?.Value;
+        if (sites == null)
+        {
+            return (null, null);
+        }
+
+        sites.RemoveAll(x => string.IsNullOrEmpty(x.DisplayName));
+
+        var spSites = new List<Site>();
+        var oneDriveSites = new List<Site>();
+
+        foreach (Site site in sites)
+        {
+            if (site == null)
+            {
+                continue;
+            }
+
+            string[]? compare = site.WebUrl?.Split(site.SiteCollection?.Hostname)[1].Split("/");
+            if (compare != null && (compare.All(x => !string.IsNullOrEmpty(x)) || compare.Length < 1))
+            {
+                continue;
+            }
+
+            if (compare?[1] == "sites" || string.IsNullOrEmpty(compare?[1]))
+            {
+                spSites.Add(site);
+            } else if (compare[1] == "personal")
+            {
+                oneDriveSites.Add(site);
+            }
+        }
+
+        return (spSites, oneDriveSites);
+
+    }
+}
